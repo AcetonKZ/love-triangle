34fadfbf4b4a947df9741a4a6c2f3209c3823e7a
AcetonKZ <ketron@mail.ru>
2019-03-21 22:40:01 +0600

    getLoveTriangelCount implemetation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
+
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+    let counter = 0;
+    for (let i = 0; i < preferences.length; i++) {
+      let firstSpichonee = preferences[i];
+      let secondSpichonee = preferences[firstSpichonee - 1];
+      let thirdSpichonee = preferences[secondSpichonee - 1];
+      if (firstSpichonee - 1 !== i && thirdSpichonee -1 === i) counter++;
+    }
+    return counter / 3;
+}
+
+
